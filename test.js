39da1855d04f454a4f740a3f4418b47c7d52ced1
Khemchand Ojha <ojha.akash876@gmail.com>
2022-05-31 14:33:43 +0530

    31/05/2022

--- test.js
+++ test.js
@@ -1,9 +1,36 @@
-const compose = (...fns) => x => fns.reduce((y, f) => f(y), x);
+/* const compose = (...fns) => x => fns.reduce((y, f) => f(y), x);
 
 const addFee = amount => amount + 2;
 const addDiscount = amount => amount - 5;
+const addSurplus = amount => amount + 10;
+const removeSurplus = amount => amount - 15;
 
-const composition = compose(addFee, addDiscount)(100);
-console.log(addFee(100));
+const composition = compose(addFee, addDiscount, addSurplus, removeSurplus)(100);
+// console.log(addFee(100));
 
-// console.log(composition);
+console.log(composition); */
+
+/* const object = {
+    val1: 1,
+    val2: 2
+};
+
+const a = Reflect.get(object, 'val1', hello);
+console.log(a);
+ */
+/* const abc = { val: 21 };
+console.log(Reflect.get(abc, "val") === 21);
+console.log(Reflect.get(abc, "x") === undefined);
+console.log(Reflect.get(abc, "y") === 21);
+
+const array1 = ['geeks1', 'geeks2', 'geeks3', 'geeks4'];
+console.log(Reflect.get(array1, 3)); */
+
+
+const foo = null ?? 'default string';
+console.log(foo);
+// expected output: "default string"
+
+const baz = false ?? 42;
+console.log(baz);
+// expected output: 0
