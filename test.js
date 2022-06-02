aa8d38119b0b3c94a7694be0fe00741ce435cf4b
Khemchand Ojha <ojha.akash876@gmail.com>
2022-06-02 10:30:38 +0530

    01/06/2022

--- test.js
+++ test.js
@@ -26,11 +26,28 @@ console.log(Reflect.get(abc, "y") === 21);
 const array1 = ['geeks1', 'geeks2', 'geeks3', 'geeks4'];
 console.log(Reflect.get(array1, 3)); */
 
-
+/* 
 const foo = null ?? 'default string';
 console.log(foo);
 // expected output: "default string"
 
 const baz = false ?? 42;
 console.log(baz);
-// expected output: 0
+
+// expected output: 0 */
+
+
+let a = 10;
+var b = 20;
+const c = 30;
+
+function z() {
+    b = 40;
+    console.log(b);
+    return b;
+}
+console.log(z());
+// console.log(d);
+console.log(b);
+// console.log(f);
+
