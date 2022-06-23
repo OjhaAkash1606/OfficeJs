640c13401990edc9f9a63472dfc6454b15b66cf0
Khemchand Ojha <ojha.akash876@gmail.com>
2022-06-23 18:01:35 +0530

    23/06/2022

---
+++ encapsulation.js
@@ -0,0 +1,36 @@
+class BankAccount {
+    customerName;
+    accountNumber;
+    balance = 0;
+
+    constructor(customerName, balance = 0) {
+        this.customerName = customerName;
+        this.balance = balance;
+        this.accountNumber = Date.now();
+    }
+    #deposite = function (amount) {
+        this.balance += amount;
+    };
+
+    withdraw(amount) {
+        this.balance -= amount;
+    }
+}
+
+
+class SavingAccount extends BankAccount {
+    transactionLimit = 10000;
+
+    constructor(customerName, balance = 0) {
+
+        super(customerName, balance);
+    }
+
+    takePersonalLoan(amount) {
+        console.log(`Taking personal loan : ${amount}`);
+    }
+}
+
+const jack = new BankAccount("Jack", 5000);
+jack.balance = 2000;
+console.log(jack);
--- test.js
+++ test.js
@@ -147,6 +147,11 @@ Person.prototype.name = function () {
 };
 
 
+console.log(Object.prototype);
+console.log(Person.prototype.__proto__);
+console.log(ankit.__proto__.__proto__);
+
+
 // console.log(Person.prototype);
 // console.log(ankit.__proto__);
 // console.log(ankit);
